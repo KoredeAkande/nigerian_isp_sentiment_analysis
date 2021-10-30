c659accb3a0c27f0e75e7499b1bdb0e294e438f4
Oluwakorede Akande <koredeakande001@gmail.com>
2021-10-30 21:21:55 +0200

    Remove credential files from github repo
    Remove credential files from github repo

--- src/credentials/alt_twitter_credentials.py
+++
@@ -1,5 +0,0 @@
-api_key = "9mJKU8LVCYiYskLEcL0UuUt9p"
-api_secret_key = "gzHV0U1rN3sw1JXg5ZgkbFmbYQUD6QaAv5Bj4TEPjMbLwPyr5b"
-access_token = "1440935989728497665-ullHx6h7crZyQ0wPUMlRXzZ8Uy1DPn"
-access_token_secret = "ea4XtiuigV0CiIF1NDFOuNUILovJw2xaEmjFxxT8gDMC1"
-
