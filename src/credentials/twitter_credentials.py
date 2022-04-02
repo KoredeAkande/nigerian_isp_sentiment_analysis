1801aa805288c7f2bf7cf7e5c2b5217965c5e5c7
Oluwakorede Akande <koredeakande001@gmail.com>
2022-04-01 20:30:25 -0700

    Added src folder

---
+++ src/credentials/twitter_credentials.py
@@ -0,0 +1,5 @@
+api_key = "orHJw0dL8ich4VNoMtyZn0cAN"
+api_secret_key = "xknoJaXNR9b15Q9h5ijaDJNIOFW7CvQL9cgKY64pZcnPSHucSc"
+access_token = "1956659575-cUcCeQHUYycW15I1eDlWzxPUcaQiDaSXl26VaAQ"
+access_token_secret = "vfI7Ke6DyTGd4XlZBshgx0lkj789Di2rxzY5GT0dzXucv"
+
